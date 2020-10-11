3a786059a3badbe3b49d2d0d6da88df5a02eab0c
Alexander Bowers <7319abowers@gmail.com>
2020-10-11 06:49:16 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,26 @@
 def find_max_value(array)
-  # Add your solution here
+  highest_index = 0
+  array.each do |current|
+    if current > highest_index
+      highest_index = current
+    end
+  end
+    highest_index
 end
+
+
+
+
+=begin
+
+array [1, 2, 3, 4, 5, 6, 3, 5, 4, 2, 0]
+take each element of the array
+create variable called highest index
+if -current iteration- > highest index
+highest index = current iteration
+
+return highest index
+
+
+
+=end
